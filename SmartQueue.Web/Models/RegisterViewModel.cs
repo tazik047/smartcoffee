bde6b08e28d3340748db3f1c4829588e6a9287a1
Stanislav Zadorozhnii <Stanislav_Zadorozhnii@epam.com>
2015-12-25 18:27:22 +0200

    add register model

---
+++ SmartQueue.Web/Models/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SmartQueue.Web.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Login { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(30)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
